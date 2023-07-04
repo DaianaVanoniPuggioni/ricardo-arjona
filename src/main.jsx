f4b7e6df42d3bcff819bee7eb5d483ac361cef7a
Guido <46787485+guidoFortunato@users.noreply.github.com>
2023-07-04 16:04:16 -0300

    agrego basename

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import InfoProvider from "./context/InfoProviders.jsx";
+import { BrowserRouter } from "react-router-dom";
 import "normalize.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
+  <BrowserRouter basename="/arjona">
     <InfoProvider>
       <App />
     </InfoProvider>
+  </BrowserRouter>
   //</React.StrictMode>
 );
