4dca738550776c50c918e5b4bed465d518a780ba
Guido <46787485+guidoFortunato@users.noreply.github.com>
2025-06-03 18:18:28 -0300

    agrego info

--- src/components/RestInformation.jsx
+++ src/components/RestInformation.jsx
@@ -1,57 +1,50 @@
+import { GrInstagram } from "react-icons/gr";
+import { FaFacebook } from "react-icons/fa";
 
 export const RestInformation = () => {
   return (
-    <div className="text-white space-y-8 mt-10">
-      {/* <div>
-        <h3 className="text-xl font-bold mb-4">SECTORES DISPONIBLES</h3>
-        <ul className="list-none space-y-4">
-          <li className="flex">
-            <span className="mr-2">-</span>
-            <div>
-              <span className="font-semibold">CAMPO</span>
-              <p className="mt-1">
-                Incluye acceso a todos los escenarios, shows, servicios,
-                activaciones y espacios gastronómicos.
-              </p>
-              <p>No incluye acceso a sector Experiencia Bandera.</p>
+    <div className="text-white space-y-8 mt-16">
+      <div>
+        <div>
+          <div className="mt-6 space-y-4">
+            <p className="font-bold text-pink-400">
+              ¿Querés conocer la grilla de artistas de Festival Bandera 2025
+              antes que nadie?
+            </p>
+            <p>
+              Comprando tus entradas sector campo en preventa hasta el
+              domingo 15 de junio, participás por accesos exclusivos al
+              evento develación de la grilla 2025.
+            </p>
+            <p>
+              Comprando tus entradas sector experiencia bandera en preventa
+              hasta el domingo 15 de junio, ya tenés tu lugar asegurado para
+              el evento develación de la grilla 2025.
+            </p>
+            <div className="flex items-center gap-2 mt-2">
+              <span className="font-bold">
+                Seguinos en redes y enterate más
+              </span>
+              <a
+                href="https://www.instagram.com/festivalbanderaok/?hl=es"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-pink-400"
+              >
+                <GrInstagram size={22} />
+              </a>
+              <a
+                href="https://www.facebook.com/festivalbanderaok"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-pink-400"
+              >
+                <FaFacebook size={22} />
+              </a>
             </div>
-          </li>
-          <li className="flex">
-            <span className="mr-2">-</span>
-            <div>
-              <span className="font-semibold">EXPERIENCIA BANDERA:</span>
-              <p className="mt-1">
-                Una manera diferente de vivir el Festival Bandera.
-              </p>
-              <ul className="list-disc pl-5 mt-1 space-y-1">
-                <li>Ingreso exclusivo al predio</li>
-                <li>
-                  Gastronomía, servicios, zona de relax y sanitarios propios
-                </li>
-                <li>
-                  Espacio diferencial al costado de los escenarios
-                  principales (no es campo delantero)
-                </li>
-                <li>
-                  Acreditación para circular libremente por las diferentes
-                  áreas habilitadas
-                </li>
-                <li>Capacidad limitada</li>
-              </ul>
-              <div className="mt-6 space-y-4">
-                <p className="font-bold text-pink-400">¿Querés conocer la grilla de artistas de Festival Bandera 2025 antes que nadie?</p>
-                <p>Comprando tus entradas sector campo en preventa hasta el domingo 15 de junio, participás por accesos exclusivos al evento develación de la grilla 2025.</p>
-                <p>Comprando tus entradas sector experiencia bandera en preventa hasta el domingo 15 de junio, ya tenés tu lugar asegurado para el evento develación de la grilla 2025.</p>
-                <div className="flex items-center gap-2 mt-2">
-                  <span className="font-bold">Seguinos en redes y enterate más</span>
-                  <a href="https://www.instagram.com/festivalbanderaok/?hl=es" target="_blank" rel="noreferrer" className="hover:text-pink-400"><GrInstagram size={22} /></a>
-                  <a href="https://www.facebook.com/festivalbanderaok" target="_blank" rel="noreferrer" className="hover:text-pink-400"><FaFacebook size={22} /></a>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div> */}
+          </div>
+        </div>
+      </div>
       <div>
         <h3 className="text-xl font-bold mb-4">MENORES</h3>
         <div className="space-y-3">
