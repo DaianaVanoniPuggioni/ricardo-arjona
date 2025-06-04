b3358cea1547040963520b489cbb0805ec889abb
guido fortunato <guido_fortunato@hotmail.com.ar>
2025-06-04 16:12:16 -0300

    cambio precio

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -4,7 +4,7 @@ export const CardsContainer = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-stretch gap-10 bg-1 mt-10">
       <div className="w-full md:w-1/3 flex">
-        <CardsPreventa3 title="CAMPO" price={70000} fee={7000} />
+        <CardsPreventa3 title="CAMPO" price={75000} fee={7500} />
       </div>
       <div className="w-full md:w-1/3 flex">
         <CardsPreventa3 title="EXPERIENCIA BANDERA" price={190000} fee={19000} />
